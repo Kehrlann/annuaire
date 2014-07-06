83ba7d64c4c08e88c510be7840aac5fe18263093
Daniel Garnier-Moiroux <git@garnier.wf>
2014-07-06 16:57:51 +0200

    Première API de recherche d'ancien (en mode fulltext).

--- annuaire_anciens/helper/jsonify.py
+++ annuaire_anciens/helper/jsonify.py
@@ -1,7 +1,14 @@
 # coding=utf-8
+
+import htmlentitydefs as entity
+
+
 def result_proxy_to_json(array=None, guillemets=False):
     """
-    Parcourir un tableau, puis mettre chaque élément dans un tableau JSON
+    Parcourir un result proxy, prendre le premier élément,
+    puis mettre chaque élément dans un tableau JSON avec des guillemets propres.
+
+    TODO : vérifier s'il ne faudrait pas plutôt utiliser json.dumps
 
     @param array: tableau
     @param guillemets: mettre des guillemets autour des termes pour une recherche exacte
@@ -25,3 +32,47 @@ def result_proxy_to_json(array=None, guillemets=False):
     else:
         result = "[]"
     return result
+
+
+def row_to_json(row):
+    """
+    Prendre une row de `sqlalchemy.ResultProxy`, et le transformer en dictionnaire,
+    avec comme clefs les noms de colonnes.
+
+    Le dictionnaire est HTML-encodé.
+
+    :param row: Une row de `sqlalchemy.ResultProxy`
+    :return: Un dictionnaire, HTML-ENCODED
+    """
+    d = {}
+    for key in row._parent.keys:
+        if isinstance(row[key], unicode):
+            d[key] = _decode_to_entity(row[key])
+        else:
+            d[key] = row[key]
+
+    return d
+
+
+
+def _decode_to_entity(u):
+    """
+    Transformer un unicode en ASCII html-encoded
+
+    Cas particulier : retourne "" si u est nul
+
+    :param u: l'entrée unicode
+    :return: u.decode('ascii', 'html') (ça n'existe pas mais #yolo).
+    """
+
+    if u is not None and not isinstance(u, unicode):
+        raise TypeError("Can only convert unicode, not {}".format(type(u)))
+
+    res = ""
+    for i in u:
+        if ord(i) in entity.codepoint2name:
+            name = entity.codepoint2name.get(ord(i))
+            res += "&" + name + ";"
+        else:
+            res += i
+    return res
