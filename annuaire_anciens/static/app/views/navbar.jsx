acbc2d63b6601685dd4bd7db58dce3a0c857422e
paduc <pad@dabla.com>
2014-07-08 11:45:37 +0200

    added the logged-in version of the navbar

--- annuaire_anciens/static/app/views/navbar.jsx
+++ annuaire_anciens/static/app/views/navbar.jsx
@@ -18,12 +18,24 @@ module.exports = React.createClass({
 
     if(!this.state.isUserConnected){
       return (
-        <LoginView>
-        </LoginView>
+        <LoginView></LoginView>
       );
     }
     else{
-      return (<div>Your are connected</div>)
+      return (
+        <div>
+          <div className="nav navbar-nav navbar-right">
+              <a className='btn btn-default navbar-btn hidden-xs' href="#">Mon compte</a>
+              <a className='btn btn-link navbar-btn hidden-xs' href="#">D&eacute;connexion</a>
+              <a className='btn btn-default navbar-btn visible-xs' href="#"><span className="glyphicon glyphicon-user"></span></a>
+              <a className='btn btn-danger navbar-btn visible-xs' href="#"><span className="glyphicon glyphicon-log-out"></span></a>
+          </div>
+          <form className="navbar-form navbar-left nav-search-zone" role="search">
+            <span className="glyphicon glyphicon-search"></span>
+            <input type="text" autocomplete="off" ref="fulltext_top" name="fulltext" className="form-control" />
+          </form>
+        </div>
+      )
     }
 
   }
