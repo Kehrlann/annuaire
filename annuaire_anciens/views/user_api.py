cff30df12904ac2081f00a878da7ff1e28067a7e
Daniel Garnier-Moiroux <git@garnier.wf>
2014-10-10 09:15:01 +0200

    Work in progress API

--- annuaire_anciens/views/user_api.py
+++ annuaire_anciens/views/user_api.py
@@ -63,11 +63,32 @@ def create_ancien_api():
 @login_required
 def update_ancien_api():
     """
-    Mettre à jour ma fiche ancien
+    Mettre à jour ma fiche ancien.
 
-    :return:
+    Input (JSON):
+    {
+        "telephone"     :   str ,
+        "mobile"        :   str ,
+        "site"          :   str ,
+        "mail_perso"    :   str
+    }
+
+    Tous les champs sont optionnels.
+
+    :return:    { "succes"  :   true/false  }
     """
-    abort(501, "Not implemented (yet) !")
+    ancien  =   _get_valid_ancien()
+    data    =   _get_valid_data()
+
+    succes = annuaire.update_fiche_ancien(
+        id_ancien   =   ancien["id_ancien"],
+        telephone   =   data.get("telephone",   ancien["telephone"]),
+        mobile      =   data.get("mobile",      ancien["mobile"]),
+        site        =   data.get("site",        ancien["site"]),
+        mail_perso  =   data.get("mail_perso",  ancien["mail_perso"])
+    )
+
+    return json.dumps({"succes":succes})
 
 
 @app.route("/api/v1/me/adresse", methods=["PUT"])
@@ -77,9 +98,28 @@ def update_adresse_api():
     Mettre à jour mon adresse. C'est une interface unifiée, même
     si techniquement, l'adresse peut ne pas exister préalablement.
 
-    :return:
+    Input (JSON) :
+    {
+        "ville"         :   str ,
+        "id_pays"       :   int ,
+        "adresse"       :   str ,
+        "code"          :   str
+    }
+
+    :return:    { "succes"  :   true/false  }
     """
-    abort(501, "Not implemented (yet) !")
+    ancien  =   _get_valid_ancien()
+    data    =   _get_valid_data()
+
+    succes  =   annuaire.update_adresse_perso(
+        id_ancien   =   ancien["id_ancien"],
+        ville       =   data.get("ville",       ""),
+        id_pays     =   data.get("id_pays",     None),
+        adresse     =   data.get("adresse",     ""),
+        code        =   data.get("code",        "")
+    )
+
+    return json.dumps({"succes":succes})
 
 
 
@@ -87,12 +127,60 @@ def update_adresse_api():
 @login_required
 def add_experience_api():
     """
-    Ajouter une expérience à un ancien
+    Ajouter une expérience à un ancien.
+
+    Input (JSON) :
+    {
+        "id_experience"     :   int             ,
+        "ville"             :   str             ,
+        "id_pays"           :   int             ,
+        "adresse"           :   str             ,
+        "code"              :   str             ,
+        "entreprise"        :   str             ,
+        "poste"             :   str             ,
+        "description"       :   str             ,
+        "mail"              :   str             ,
+        "site"              :   str             ,
+        "telephone"         :   str             ,
+        "mobile"            :   str             ,
+        "date_debut"        :   date (MM/yyyy)  ,
+        "date_fin"          :   date (MM/yyyy)
+    }
 
     :return:
     """
-    abort(501, "Not implemented (yet) !")
-
+    ancien  =   _get_valid_ancien()
+    data    =   _get_valid_data()
+    # TODO
+    # TODO
+    # TODO
+    # TODO
+    # TODO
+    # TODO
+    # TODO
+    # TODO
+    # TODO
+    # TODO
+    # TODO
+    # TODO
+    # TODO
+    succes  =   annuaire.update_experience(
+        id_ancien,
+        id_experience,
+        ville,
+        id_pays,
+        adresse,
+        code,
+        entreprise,
+        poste,
+        description,
+        mail,
+        site,
+        telephone,
+        mobile,
+        date_debut,
+        date_fin=None,
+    )
 
 
 @app.route("/api/v1/me/experience/<int:id_experience>", methods=["PUT"])
@@ -141,33 +229,36 @@ def delete_experience_api(id_experience):
     abort(501, "Not implemented (yet) !")
 
 
-
-def _is_this_me(id_ancien):
+def _get_valid_ancien():
     """
-    Vérifier que l'ancien modifié est bien l'ancien
-    qui fait le call ...
+    Récupérer l'ancien associé à l'utilisateur courant.
 
-    Sinon, abort avec une 403 Forbidden
+    Si l'ancien n'existe pas,   abort   404
+    Si l'ancien est bloqué,     abort   403 (??)
 
-    :param int id_ancien:   L'id de l'ancien en question
-    :return:                Abort si illegal, None si autre
+    :return: L'ancien :o)
     """
-    if current_user.id_ancien != id_ancien:
-        abort(403, "Tu n'as pas le droit de modifier la fiche d'un autre ancien !")
-
-
-def _is_not_blocked(ancien):
-    """
-    Vérifier que l'ancien modifié n'est pas bloqué ....
-
-    Sinon, abort avec une 403 Forbidden
-
-    :param int id_ancien:   L'id de l'ancien en question
-    :return:                Abort si illegal, None si autre
-    """
-    if ancien is not None and ancien["bloque"]:
-        # TODO : bloqué
-        # TODO : quel code retour ?
+    ancien = annuaire.find_ancien_by_id(current_user.id_ancien, actif=None, nouveau=False, bloque=None)
+    if ancien is None:
+        abort(404, "Pas d'ancien associe a ce compte.")
+    elif ancien["bloque"]:
         abort(403, "Il semblerai que tu compte soit bloque. "
-                   "Merci de contacter un administrateur pour le "
-                   "faire debloquer.")
+           "Merci de contacter un administrateur pour le "
+           "faire debloquer.")
+    else:
+        return ancien
+
+
+def _get_valid_data():
+    """
+    Récupérer des données qui ont été envoyées (...) au format JSON.
+
+    Si le parser plante : abort 400
+
+    :return:    Les données sous forme de dictionnaire.
+    """
+    try:
+        return json.loads(request.data)
+    except:
+        abort(400, "Les donnees n'ont pas ete envoyees au format JSON valide.")
+
